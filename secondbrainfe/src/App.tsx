e68a951c35d469bfbde52aed9554b298e7307dc7
aadityap0ap <adityabharti450ap@gmail.com>
2026-04-30 11:10:18 +0530

    React Routing Done

--- secondbrainfe/src/App.tsx
+++ secondbrainfe/src/App.tsx
@@ -1,11 +1,17 @@
 //import Dashboard from "./pages/Dashboard"
-import { Signin } from "./pages/Signin"
+//import { Signin } from "./pages/Signin"
 //import { Signup } from "./pages/Signup"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Signup } from "./pages/Signup"
+import { Signin } from "./pages/Signin"
+import Dashboard from "./pages/Dashboard"
 
 export default function App(){
-  return(
-  <div>
-    <Signin/>
-  </div>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/signup" element={<Signup/>} />
+    <Route path="/signin" element={<Signin/>}/>
+    <Route path="/dashboard" element={<Dashboard/>}/>
+  </Routes>
+  </BrowserRouter>
 }
